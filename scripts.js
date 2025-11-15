e0edbe6670c08610fc4c2d3230c9fb4dfd62ec5d
Oyedokun Oluwamuyiwa <oyedokunolumuyiwa07@gmail.com>
2025-11-15 20:47:04 +0100

    commit

--- scripts.js
+++ scripts.js
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const scoreTxt = document.querySelector('.scoreId');
   const percentTxt = document.querySelector('.percentId');
   const restartBtn = document.querySelector('.restart_btn');
-  const answerScreen = document.querySelector('.answer-screen');
 
   let selectedQuestion;
   let currentQuestionObject = 0;
@@ -276,7 +275,6 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     return userAnswer;
   }
-
   nextBtn.addEventListener('click', function () {
     const answerResult = checkAnswer();
 
